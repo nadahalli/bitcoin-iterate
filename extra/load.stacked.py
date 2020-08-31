8c0003e636c3a9ecb2db2301c81e50c695d670ca
Tejaswi Nadahalli <nadahalli@gmail.com>
2020-08-31 23:07:33 +0200

    Moved churn removal to graphing, and not processing

--- extra/load.stacked.py
+++ extra/load.stacked.py
@@ -1,30 +1,84 @@
+import sys
 import pylab as plt
 import pickle
 from collections import defaultdict
 
-X = pickle.load(open("labels.pkl", "rb"))
-Y = pickle.load(open("values.pkl", "rb"))
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--percentage',
+                      type=eval, 
+                      choices=[True, False], 
+                      default='False')
+parser.add_argument('--compress',
+                      type=eval, 
+                      choices=[True, False], 
+                      default='False')
+
+args = parser.parse_args()
+
+X = pickle.load(open("labels.btc.pkl", "rb"))
+Y = pickle.load(open("values.btc.pkl", "rb"))
 max_labels = 20
+gap = 1024
 
 pY = defaultdict(list)
 
 for i, x in enumerate(X):
     sum = 0
+    sum_compress = 0
     for j, y in enumerate(Y):
         sum += y[i]
+        if j in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+            sum_compress += y[i]/(gap/pow(2, j))
+
+    compression_done = False
     for j, y in enumerate(Y):
-        pY[j].append(100.0 * y[i]/sum)
+        to_add = y[i]
+        if args.compress:
+            if j in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+                if not compression_done:
+                    to_add = sum_compress
+                    compression_done = True
+                    if args.percentage:
+                        pY[j].append(100.0 * to_add/sum)
+                    else:
+                        pY[j].append(to_add)
+            else:
+                if args.percentage:
+                    pY[j].append(100.0 * to_add/sum)
+                else:
+                    pY[j].append(to_add)
+
+        else:
+            if args.percentage:
+                pY[j].append(100.0 * to_add/sum)
+            else:
+                pY[j].append(to_add)
+
+for key, value in pY.items():
+    print(key, len(value))
 
 nY = []
-for key, value in sorted(pY.items()):
+labels = []
+nY.append(pY[0])
+labels.append(0)
+for key, value in sorted(pY.items(), reverse=True):
+    if key == 0:
+        continue
     nY.append(value)
+    labels.append(key)
+    
 
 fig, ax = plt.subplots()
-ax.stackplot(X, *nY, labels=[i for i in range(0, max_labels)])
+ax.stackplot(X, *nY, labels=labels)
 ax.legend(loc='upper left')
     
 box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+handles, labels = ax.get_legend_handles_labels()   #get the handles
+ax.legend(reversed(handles), reversed(labels))
+
 plt.show()
     
--- extra/stacked.py
+++ extra/stacked.py
@@ -6,7 +6,7 @@ import sys
 import time
 
 gap = 1024
-labels = set([])
+max_labels = 20
 
 X = []
 Ys = defaultdict(list)
@@ -20,16 +20,10 @@ new_chunk = True
 for line in sys.stdin:
     block, life, amount, coinbase = map(int, line.split(','))
     amount = amount / 10000000
-    print(block, block%gap, new_chunk)
     if block % gap == 0 and new_chunk:
-        print("here")
         new_chunk = False
-        for label in range(0, max(labels)):
-            amount_per_label = gap_ys.get(label, 0)
-            proportional_amount_per_label = amount_per_label
-            if label != 0 and label < mylog(gap):
-                proportional_amount_per_label = amount_per_label/(gap/pow(2, label))
-            Ys[label].append(proportional_amount_per_label)
+        for label in range(0, max_labels):
+            Ys[label].append(gap_ys.get(label, 0))
         gap_ys.clear()
         X.append(block - gap)
         print(time.asctime(), block - gap)
@@ -37,7 +31,6 @@ for line in sys.stdin:
         new_chunk = True
 
     label = mylog(life + 2)
-    labels.add(label)
     gap_ys[label] += amount
 
 for key, value in sorted(Ys.items()):
@@ -47,7 +40,6 @@ Y = []
 for key, value in sorted(Ys.items()):
     Y.append(value)
 
-"""
 import pickle
 
 with open('labels.btc.pkl', 'wb') as f:
@@ -56,11 +48,10 @@ with open('values.btc.pkl', 'wb') as f:
     pickle.dump(Y, f)    
 
 fig, ax = plt.subplots()
-ax.stackplot(X, *Y, labels=[i for i in range(0, max(labels))])
+ax.stackplot(X, *Y, labels=[i for i in range(0, max_labels)])
 ax.legend(loc='upper left')
     
 box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
-"""

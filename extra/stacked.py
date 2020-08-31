72b2ed2ab3a2735676c5fc651f9a16fdf96123b4
Tejaswi Nadahalli <nadahalli@gmail.com>
2020-08-31 13:31:54 +0200

    Proportional Amounts

--- extra/stacked.py
+++ extra/stacked.py
@@ -5,28 +5,40 @@ from collections import defaultdict
 import sys
 import time
 
-max_block = 644276
-gap = 144 * 7
-max_labels = 20
+gap = 1024
+labels = set([])
 
 X = []
 Ys = defaultdict(list)
 gap_ys = defaultdict(int)
 
+def mylog(x):
+    return math.floor(math.log(x, 2))
+
+new_chunk = True
+
 for line in sys.stdin:
-    block, life = map(int, line.split(','))
-    if block % gap == 0 and new_block:
-        new_block = False
-        for label in range(0, max_labels):
-            Ys[label].append(gap_ys.get(label, 0))
+    block, life, amount, coinbase = map(int, line.split(','))
+    amount = amount / 10000000
+    print(block, block%gap, new_chunk)
+    if block % gap == 0 and new_chunk:
+        print("here")
+        new_chunk = False
+        for label in range(0, max(labels)):
+            amount_per_label = gap_ys.get(label, 0)
+            proportional_amount_per_label = amount_per_label
+            if label != 0 and label < mylog(gap):
+                proportional_amount_per_label = amount_per_label/(gap/pow(2, label))
+            Ys[label].append(proportional_amount_per_label)
         gap_ys.clear()
         X.append(block - gap)
         print(time.asctime(), block - gap)
     if block % gap != 0:
-        new_block = True
+        new_chunk = True
 
-    bucket = math.floor(math.log(life + 2, 2))
-    gap_ys[bucket] += 1
+    label = mylog(life + 2)
+    labels.add(label)
+    gap_ys[label] += amount
 
 for key, value in sorted(Ys.items()):
     print(key, len(value))
@@ -35,18 +47,20 @@ Y = []
 for key, value in sorted(Ys.items()):
     Y.append(value)
 
+"""
 import pickle
 
-with open('labels.pkl', 'wb') as f:
+with open('labels.btc.pkl', 'wb') as f:
     pickle.dump(X, f)
-with open('values.pkl', 'wb') as f:
+with open('values.btc.pkl', 'wb') as f:
     pickle.dump(Y, f)    
 
 fig, ax = plt.subplots()
-ax.stackplot(X, *Y, labels=[i for i in range(0, max_labels)])
+ax.stackplot(X, *Y, labels=[i for i in range(0, max(labels))])
 ax.legend(loc='upper left')
     
 box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
+"""
